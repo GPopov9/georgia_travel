2efc2ebd40159dd764c7c53bd97d5be015504073
George <g.popov44@icloud.com>
2021-03-25 00:02:43 +0300

    +router

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,6 +3,7 @@ import CardItem from './CardItem';
 import './Cards.css';
 
 import image1 from '../images/img-9.jpg';
+import image2 from '../images/img-2.jpg';
 
 const Cards = () => (
   <div className="cards">
@@ -14,6 +15,32 @@ const Cards = () => (
             src={image1}
             text="Explore the hidden waterfall"
             label="Adventure"
+            path="/services"
+          />
+          <CardItem
+            src={image2}
+            text="Travel through the Islads"
+            label="Luxury"
+            path="/services"
+          />
+        </ul>
+        <ul className="cards__items">
+          <CardItem
+            src={image1}
+            text="Explore the hidden waterfall"
+            label="Adventure"
+            path="/services"
+          />
+          <CardItem
+            src={image2}
+            text="Travel through the Islads"
+            label="Luxury"
+            path="/services"
+          />
+          <CardItem
+            src={image2}
+            text="Travel through the Islads"
+            label="Luxury"
             path="/services"
           />
         </ul>
---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+const Services = () => (
+  <h1 className="services">SERVICES</h1>
+);
+
+export default Services;
